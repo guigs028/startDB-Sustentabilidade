1e8a985bc075e8a94b2002352da4b5ea609a8f79
guigs <guidentzien@hotmail.com>
2025-11-22 21:29:50 -0300

    Primeiro commit

---
+++ src/main/java/com/ecodb/eco_points/EcoPointsApplication.java
@@ -0,0 +1,13 @@
+package com.ecodb.eco_points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecodb/eco_points/EcoPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodb.eco_points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
