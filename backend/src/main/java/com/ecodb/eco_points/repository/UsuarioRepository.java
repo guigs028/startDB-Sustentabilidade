1b9f22e09995df9ea0002f2e51a98152a6b01262
LeonardoEnnes <leonardoennes@protonmail.com>
2025-11-26 16:39:38 -0300

    feat: implement user registration functionality

--- backend/src/main/java/com/ecodb/eco_points/repository/UsuarioRepository.java
+++ backend/src/main/java/com/ecodb/eco_points/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ecodb.eco_points.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+    boolean existsByEmail(String email);
 }
