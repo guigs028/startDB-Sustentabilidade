63af5e7280fb2e9407fa2a234dbabc191d24b6a3
LeonardoEnnes <leonardoennes@protonmail.com>
2025-12-03 11:20:21 -0300

    feat: add CORS configuration

--- backend/src/main/java/com/ecodb/eco_points/config/SecurityConfig.java
+++ backend/src/main/java/com/ecodb/eco_points/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.ecodb.eco_points.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +40,26 @@ public class SecurityConfig {
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource () {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // foi passado como uma lista mas dá para usar * para permitir tudo
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000")); 
+        
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        
+        config.setAllowedHeaders(List.of("*"));
+        
+        // auth
+        config.setAllowCredentials(true); 
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
     @Bean
